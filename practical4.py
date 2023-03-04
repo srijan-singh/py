822a2a34fc2fff969485b288875df07afe47e0ed
srijan-singh <srijanssj5goku@gmail.com>
2023-03-04 14:31:43 +0530

    Practical 4

---
+++ practical4.py
@@ -0,0 +1,9 @@
+num1 = int(input("First Number:"))
+num2 = int(input("Second Number:"))
+
+print(f"Before Swapping {num1} {num2}")
+
+num1, num2 = num2, num1
+
+print(f"After Swapping {num1} {num2}")
+
